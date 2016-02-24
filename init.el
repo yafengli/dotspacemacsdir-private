5f0f202f2862cd645d83db1373e91886abad16a9
ya_feng_li <ya_feng_li@163.com>
2016-02-24 20:11:29 +0800

    init.el

--- init.el
+++ init.el
@@ -36,7 +36,7 @@ values."
      asciidoc
      markdown
      org
-     html
+     ;; html
      ;; My layer
      yafengli
      ;; (shell :variables
@@ -205,7 +205,10 @@ values."
   "Initialization function for user code.
 It is called immediately after `dotspacemacs/init'.  You are free to put any
 user code."  
-  )
+   ;;(setq url-proxy-services
+	;;'(("http" . "127.0.0.1:8989")
+	  ;;("https" . "127.0.0.1:8989")))
+)
 
 (defun dotspacemacs/user-config ()
   "Configuration function for user code.
