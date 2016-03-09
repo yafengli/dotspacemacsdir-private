31a522fb47c916f9e9b3a62184f75bc599ad40ce
ya_feng_li <ya_feng_li@163.com>
2016-03-09 11:02:11 +0800

    int

--- init.el
+++ init.el
@@ -24,8 +24,7 @@ values."
      ;; <M-m f e R> (Emacs style) to install them.
      ;; ----------------------------------------------------------------
      (auto-completion :variables auto-completion-enable-sort-by-usage t)
-     better-defaults
-     better-defaults
+     better-defaults     
      emacs-lisp
      scala
      clojure
@@ -216,7 +215,7 @@ values."
    ;; If non nil smooth scrolling (native-scrolling) is enabled. Smooth
    ;; scrolling overrides the default behavior of Emacs which recenters the
    ;; point when it reaches the top or bottom of the screen. (default t)
-   ;; dotspacemacs-smooth-scrolling t
+   ;; /dotspacemacs-smooth-scrolling t
    ;; If non nil line numbers are turned on in all `prog-mode' and `text-mode'
    ;; derivatives. If set to `relative', also turns on relative line numbers.
    ;; (default nil)
