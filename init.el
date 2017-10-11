8d545bad1c4e1118865bb550d9fc4f9cad8c112d
ya_feng_li <ya_feng_li@163.com>
2017-10-11 10:43:16 +0800

    add line font.

--- init.el
+++ init.el
@@ -127,7 +127,8 @@ values."
    ;; size to make separators look not too crappy.
    dotspacemacs-default-font '(;;"Source Code Pro"
                                ;;"Lucida Console"
-                               "DejaVu Sans Mono"
+                               ;;"DejaVu Sans Mono"
+                               "ProFontWindows"
                                :size 12
                                :weight normal
                                :width normal
@@ -259,7 +260,7 @@ values."
   "Initialization function for user code.
 It is called immediately after `dotspacemacs/init'.  You are free to put almost
 any user code here.  The exception is org related code, which should be placed
-in `dotspacemacs/user-config'."  
+in `dotspacemacs/user-config'."  	
   )
 
 (defun dotspacemacs/user-config ()
--- layers/yafengli/config.el
+++ layers/yafengli/config.el
@@ -8,11 +8,9 @@
 ;; 在标题栏提示你目前在什么位置
 (setq frame-title-format "ya_feng_li@163.com")
 ;; 显示行数
-(global-linum-mode t)
-;; 符号成对
-;;(electric-pair-mode t)
+(setq-default dotspacemacs-line-numbers t)
 ;; 全屏
-(run-with-idle-timer 0.1 nil 'emacs-maximize)
+(run-with-idle-timer 1 nil 'emacs-maximize)
 (add-to-list 'default-frame-alist '(fullscreen . fullboth))
 ;;启动单进程Server
 (server-start)
