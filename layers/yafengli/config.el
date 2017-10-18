2aab114b0c128a3911189d3a315bc8500998e8ee
ya_feng_li <ya_feng_li@163.com>
2017-10-18 14:00:29 +0800

    ok.

--- layers/yafengli/config.el
+++ layers/yafengli/config.el
@@ -11,6 +11,12 @@
 (setq-default dotspacemacs-line-numbers t)
 ;; tab长度2
 (setq default-tab-width 2)
+;;
+(display-time-mode t)
+;;时间的变化频率
+(setq display-time-interval 10)
+;;显示时间的格式
+(setq display-time-format "%m月%d日%A%H:%M")
 ;; 全屏
 ;;(run-with-idle-timer 1 nil 'emacs-maximize)
 (add-to-list 'default-frame-alist '(fullscreen . fullboth))
